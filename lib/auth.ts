579018085b79f74af12a740c3f9988505742cfb1
Comradenomad <ethandeetee@gmail.com>
2026-04-26 00:06:29 -0700

    Remove Google auth for demo flow

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,10 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 import { getDatabase } from "./mongodb"
 import bcrypt from "bcryptjs"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
     Credentials({
       name: "credentials",
       credentials: {
